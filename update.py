95e83f23a158a0134f4407b0c76c3aa632b616ec
ngc7331 <ngc7331@outlook.com>
2022-04-15 12:55:09 +0800

    feat: get subprof_dict from uniequip_table

--- update.py
+++ update.py
@@ -22,39 +22,6 @@ OUTPUT = {
     'profs': 'profs.json'
 }
 
-def getName(s:str):
-    prof_name = {
-        'support': '辅助',
-        'summoner': '召唤师',
-
-        'warrior': '近卫',
-        'instructor': '教官',
-        'sword': '剑豪',
-        'fearless': '无畏者',
-
-        'sniper': '狙击',
-        'longrange': '神射手',
-        'aoesniper': '炮手',
-
-        'medic': '医疗',
-        'ringhealer': '群愈师',
-
-        'special': '特种',
-        'executor': '处决者',
-        'merchant': '行商',
-
-        'caster': '术师',
-        'chain': '链术师',
-        'splashcaster': '扩散术师',
-
-        'pioneer': '先锋',
-        'charger': '冲锋手'
-    }
-    s = s.lower()
-    if s not in prof_name.keys():
-        return s
-    return prof_name[s]
-
 
 def write(path:str, file:str, data:dict):
     if not os.path.exists(path): os.mkdir(path)
@@ -67,6 +34,17 @@ if __name__ == '__main__':
     char_equip = uniequip_table['charEquip']
     equip_dict = uniequip_table['equipDict']
     mission_list = uniequip_table['missionList']
+    subprof_dict = {key: value['subProfessionName'] for key, value in uniequip_table['subProfDict'].items()}
+
+    prof_dict = {
+        'support': '辅助',
+        'warrior': '近卫',
+        'sniper': '狙击',
+        'medic': '医疗',
+        'special': '特种',
+        'caster': '术师',
+        'pioneer': '先锋',
+    }
 
     character_table = json.loads(requests.get('{BASE_URL}/{SERVER}/{CHARACTER_TABLE}'.format(**CONST)).content)
 
@@ -96,12 +74,12 @@ if __name__ == '__main__':
 
         if prof not in profs.keys():
             profs[prof] = {
-                'name': getName(prof),
+                'name': prof_dict[prof.lower()],
                 'sub': {}
             }
         if subprof not in profs[prof]['sub'].keys():
             profs[prof]['sub'][subprof] = {
-                'name': getName(subprof),
+                'name': subprof_dict[subprof],
                 'show': True
             }
 
