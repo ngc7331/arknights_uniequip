b9dcbbf22b0b602f9474024f82398771e29c04e4
ngc7331 <ngc7331@outlook.com>
2022-05-01 20:43:18 +0800

    feat: run.sh throws error when python fails

--- run.sh
+++ run.sh
@@ -2,6 +2,11 @@
 for s in $@
 do
     python update.py -s $s
+    ret=$?
+    if [ $ret -ne 0 ]; then
+        echo "python run failed with exit code $ret"
+        exit $ret
+    fi
     git add data/$s/*.json
     git commit -m "update data/$s" || echo nothing to commit
 done
