2be3126396c17d68806838d31d3463eb66ba8995
ngc7331 <ngc7331@outlook.com>
2022-07-01 17:29:04 +0800

    feat(run.sh): prompts

--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 for s in $@
 do
+    echo "=== updating $s ==="
+    echo "-> run script"
     python update.py -s $s
     ret=$?
     if [ $ret -ne 0 ]; then
         echo "python run failed with exit code $ret"
-        exit $ret
+        continue
     fi
+    echo "-> commit"
     git add data/$s/*.json
-    git commit -m "update data/$s" || echo nothing to commit
+    git commit -m "update data/$s" && echo "..done" || echo "..nothing to commit"
 done
